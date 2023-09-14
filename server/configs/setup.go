71e24c94fdf6dd3d227b7d5fc3400bf500d10a70
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2023-09-14 23:14:58 +0800

    Update setup.go

--- server/configs/setup.go
+++ server/configs/setup.go
@@ -1,40 +1,36 @@
 package configs
 
 import (
-    "context"
-    "fmt"
-    "log"
-    "time"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	"context"
+	"fmt"
+	"log"
+
+	// "time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func ConnectDB() *mongo.Client  {
-    client, err := mongo.NewClient(options.Client().ApplyURI(EnvMongoURI()))
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
-    err = client.Connect(ctx)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    //ping the database
-    err = client.Ping(ctx, nil)
-    if err != nil {
+func ConnectDB() *mongo.Client {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(EnvMongoURI()))
+	if err != nil {
 		log.Fatal(err)
-    }
-    fmt.Println("Connected to MongoDB")
-    return client
+	}
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB")
+	return client
 }
 
-//Client instance
+// Client instance
 var DB *mongo.Client = ConnectDB()
 
-//getting database collections
+// getting database collections
 func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
-    collection := client.Database("AlgoVisual").Collection(collectionName)
-    return collection
+	collection := client.Database("AlgoVisual").Collection(collectionName)
+	return collection
 }
