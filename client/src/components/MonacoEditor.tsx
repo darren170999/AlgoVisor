f2efdebb32056609dddb5036edef04b6cdc2ceae
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2023-11-09 13:53:17 +0800

    Update MonacoEditor.tsx

--- client/src/components/MonacoEditor.tsx
+++ client/src/components/MonacoEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Editor } from "@monaco-editor/react";
 import axios from "axios";
 
@@ -20,85 +20,57 @@ function MonacoEditor() {
   const file = files[fileName];
   const editorRef = useRef<any>(null);
   const [output, setOutput] = useState<string>("");
-
+    function waitFor3second(){
+        return new Promise(resolve =>
+            setTimeout(() => resolve("result"),3000)
+        );
+    }
   function handleEditorDidMount(editor: any, monaco: any) {
     editorRef.current = editor;
   }
+useEffect(()=>{
 
-  async function compileAndRunCode() {
+},[output])
+  function compileAndRunCode() {
     if (editorRef.current) {
       const codeToCompile: string = editorRef.current.getValue();
-  
-      try {
-        const response = await axios.post("http://0.0.0.0:2358/submissions", {
+
+      // Make a POST request to Judge0 to compile and run the code.
+      axios
+        .post("http://0.0.0.0:2358/submissions", {
           source_code: codeToCompile,
           language_id: 71,
+        })
+        .then((response) => {
+          // Handle the response from Judge0, which will include the token.
+          const submissionToken: string = response.data.token;
+          waitFor3second().then(()=>
+          // Poll Judge0 for the result (you can implement this as needed).
+            {pollJudge0ForResult(submissionToken);}
+          )
+        })
+        .catch((error) => {
+          console.error("Error compiling code:", error);
         });
-  
-        // Handle the response from Judge0, which will include the token.
-        const submissionToken: string = response.data.token;
-  
-        // Wait for the result and set the output
-        await pollJudge0ForResult(submissionToken);
-      } catch (error) {
-        console.error("Error compiling code:", error);
-      }
     }
   }
-  
-  async function pollJudge0ForResult(submissionToken: string) {
-    try {
-      while (true) {
-        const response = await axios.get(
-          `http://0.0.0.0:2358/submissions/${submissionToken}`
-        );
-  
-        const status: string = response.data.status.description;
-  
-        if (status === "Processing") {
-          // Submission is still processing; continue polling.
-          await new Promise((resolve) => setTimeout(resolve, 10000));
-        } else if (status === "Accepted") {
-          // Submission is accepted, retrieve the output.
+
+  function pollJudge0ForResult(submissionToken: string) {
+    // Implement a function to poll Judge0 for the result of the submission.
+    // You can use a timer or WebSocket to check the status of the submission and get the output.
+    // Example code for polling Judge0:
+    axios
+      .get(`http://0.0.0.0:2358/submissions/${submissionToken}`)
+      .then((response) => {
+            console.log(response)
           const submissionOutput: string = response.data.stdout;
           setOutput(submissionOutput);
-          break; // Exit the loop once the output is available.
-        } else {
-          // Handle other submission statuses as needed.
-          console.error("Submission status:", status);
-          break; // Exit the loop on other statuses as well.
-        }
-      }
-    } catch (error) {
-      console.error("Error polling Judge0:", error);
-    }
+    //     if (status === "Processing") {
+    //       // Submission is still processing; continue polling.
+    //       setTimeout(() => pollJudge0ForResult(submissionToken), 1000);
+    //     }
+      });
   }
-
-//   function pollJudge0ForResult(submissionToken: string) {
-//     // Implement a function to poll Judge0 for the result of the submission.
-//     // You can use a timer or WebSocket to check the status of the submission and get the output.
-//     // Example code for polling Judge0:
-//     axios
-//       .get(`http://0.0.0.0:2358/submissions/${submissionToken}`)
-//       .then((response) => {
-//         const status: string = response.data.status.description;
-
-//         if (status === "Processing") {
-//           // Submission is still processing; continue polling.
-//           setTimeout(() => pollJudge0ForResult(submissionToken), 10000);
-//         } else if (status === "Accepted") {
-//           // Submission is accepted, retrieve the output.
-//           const submissionOutput: string = response.data.stdout;
-//           setOutput(submissionOutput);
-//         } else {
-//           // Handle other submission statuses as needed.
-//           console.error("Submission status:", status);
-//         }
-//       })
-//       .catch((error) => {
-//         console.error("Error polling Judge0:", error);
-//       });
-//   }
 
   return (
     <>
