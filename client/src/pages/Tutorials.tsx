f1ca7f979f8c13ce4d6278da1c7c2b9ad101b4c0
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2024-01-19 13:06:34 +0800

    Filter by tags
    - Tutorial Questions have been filtered by tags.
    - Appropriate questions will show up at appropriate tabs

--- client/src/pages/Tutorials.tsx
+++ client/src/pages/Tutorials.tsx
@@ -13,85 +13,86 @@ type QnType = {
   qnid: string;
 };
 function Tutorials(){
-    //PUT IN DB, Call everything in Tutorials one time and filter. Status is all new. 
-    const [questions, setQuestions] = useState<QnType[]>([]);
-    const getQuestions = async() => {
-
-      console.log("accessing");
-      try{
-          const response = await fetch("http://localhost:8080/tutorials", {
-              method: "GET",
-              headers: {
-                  "Content-Type": "application/json",
-              }
-          });
-          if(response.ok){
-              console.log(response);
-              var res = await response.json();
-              setQuestions(res.data.data)
-              
-          }
-      } catch {
-          console.log("Failed")
-      }
+  //PUT IN DB, Call everything in Tutorials one time and filter. Status is all new. 
+  const [questions, setQuestions] = useState<QnType[]>([]);
+  const getQuestions = async() => {
+    console.log("accessing");
+    try{
+        const response = await fetch("http://localhost:8080/tutorials", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        if(response.ok){
+            console.log(response);
+            var res = await response.json();
+            setQuestions(res.data.data)
+            
+        }
+    } catch {
+        console.log("Failed")
+    }
   }
   useEffect(()=>{
       getQuestions()
+      console.log(questions)
   },[])
-    return(
-        <>
-        <Header></Header>
-        <FullScreenSection backgroundColor="#1a1f71" alignItems="center" flex-direction="row"
-        isDarkBackground p={8}  spacing={8}>
-        <VStack>
+
+  const appropriateTabs = (tabIndex: number) => {
+    const tabName = `Tut${tabIndex + 1}`;
+    return questions.filter((question) => question.tags === tabName)
+    .map((question)=> (
+    <TutorialQuestion
+    key={question.qnid}
+    qnid={question.qnid}
+    status={question.status}
+    tags={question.tags}
+    description={question.description}
+    name={question.name}/>
+    ));
+  }
+
+  return(
+      <>
+      <Header></Header>
+      <FullScreenSection backgroundColor="#1a1f71" alignItems="center" flex-direction="row"
+      isDarkBackground p={8}  spacing={8}>
+      <VStack>
+        {" "}
+        <Avatar size='2xl' src='/logo192.png'>
+          {" "}{" "}
+        </Avatar>
+        <h1 style={{color: "#F7B600"}}></h1>
+        <Heading>
           {" "}
-          <Avatar size='2xl' src='/logo192.png'>
-            {" "}{" "}
-          </Avatar>
-          <h1 style={{color: "#F7B600"}}></h1>
-          <Heading>
-            {" "}
-            <h1 style={{textAlign: "center"}}>Welcome to the new IE2108 Tutorials</h1>
-            <h1 style={{color: "#F7B600"}}>Please attempt them every week</h1>
-            {" "}
-          </Heading>
-        </VStack>
-        <VStack>
-        <Tabs variant='enclosed' color={"yellow"} maxWidth=''>
-            <TabList>
-                <Tab>Tut 1</Tab>
-                <Tab>Tut 2</Tab>
-                <Tab>Tut 3</Tab>
-                <Tab>Tut 4</Tab>
-                <Tab>Tut 5</Tab>
-                <Tab>Tut 6</Tab>
-                <Tab>Tut 7</Tab>
-                <Tab>Tut 8</Tab>
-                <Tab>Tut 9</Tab>
-                <Tab>Tut 10</Tab>
-                <Tab>Tut 11</Tab>
-                <Tab>Tut 12</Tab>
-            </TabList>
-            <TabPanels>
-              <TabPanel>
-                <Box overflowX='auto' w= '100%' maxWidth='900px'> 
-                  <HStack spacing={4}  style={{ flexWrap: 'nowrap' }}>
-                  {questions.map((question) => (
-                      <TutorialQuestion key={question.qnid} qnid ={question.qnid} status={question.status} tags={question.tags}
-                      description={question.description} name={question.name}/>
-                  ))}
-                  {/* Write some logic to allow the tags to flow properly,  */}
-                  </HStack>                 
-                </Box>
-              </TabPanel>
-              <TabPanel>
-                <p>two!</p>
-              </TabPanel>
-            </TabPanels>
-        </Tabs>
-        </VStack>
-        </FullScreenSection>
-        </>
-    );
+          <h1 style={{textAlign: "center"}}>Welcome to the new IE2108 Tutorials</h1>
+          <h1 style={{color: "#F7B600"}}>Please attempt them every week</h1>
+          {" "}
+        </Heading>
+      </VStack>
+      <VStack>
+      <Tabs variant='enclosed' color={"yellow"} maxWidth=''>
+        <TabList>
+          {[...Array(12).keys()].map((index) => (
+            <Tab key={index}>Tutorial {index + 1}</Tab>
+          ))}
+        </TabList>
+        <TabPanels>
+          {[...Array(12).keys()].map((index) => (
+                <TabPanel key={index} className={`Tut${index + 1}`}>
+                  <Box overflowX="auto" w="100%" maxWidth="900px">
+                    <HStack spacing={4} style={{ flexWrap: "nowrap" }}>
+                      {appropriateTabs(index)}
+                    </HStack>
+                  </Box>
+                </TabPanel>
+              ))}
+        </TabPanels>
+      </Tabs>
+      </VStack>
+      </FullScreenSection>
+      </>
+  );
 }
 export default Tutorials;
