0b355817eeeea13318ca5acc8d75fffa85249e1c
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2024-01-30 10:24:41 +0800

    Fix bugs
    - Bugs in the URL parameters and Swagger configs are now fix, issue: Typo

--- server/models/attempt_model.go
+++ server/models/attempt_model.go
@@ -5,10 +5,10 @@ import (
 )
 
 type Attempt struct {
-	Id      primitive.ObjectID `json:"id,omitempty"`
+	Id       primitive.ObjectID `json:"id,omitempty"`
 	QnId     string             `json:"qnid,omitempty" validate:"required"`
 	Username string             `json:"username,omitempty" validate:"required"`
 	Attempt  string             `json:"attempt,omitempty" validate:"required"`
-	Language string             `json:"language,omitempty" validate:"required"`
+	Language int                `json:"language,omitempty" validate:"required"`
 	Status   string             `json:"status,omitempty" validate:"required"`
 }
--- server/routes/attempt_routes.go
+++ server/routes/attempt_routes.go
@@ -8,12 +8,12 @@ import (
 
 func AttemptRoute(router *gin.Engine) {
 	//All routes related to users comes here
-	router.POST("/tutorials/code/attempt/:qnid", controllers.CreateAttempt())
+	router.POST("/tutorials/code/attempt/create", controllers.CreateAttempt())
 	router.GET("/tutorials/code/attempt/:qnid", controllers.GetAttemptByQnIdByUsername())
 	router.PUT("/tutorials/code/attempt/:qnid", controllers.UpdateAttempt())
 	router.GET("/tutorials/code/attempt", controllers.GetAllAttempts())
 	// Swagger API testing
-	router.POST("/api//tutorials/code/attempt/:qnid", controllers.CreateAttempt())
+	router.POST("/api/tutorials/code/attempt/create", controllers.CreateAttempt())
 	router.GET("/api/tutorials/code/attempt/:qnid", controllers.GetAttemptByQnIdByUsername())
 	router.PUT("/api/tutorials/code/attempt/:qnid", controllers.UpdateAttempt())
 	router.GET("/api/tutorials/code/attempt", controllers.GetAllAttempts())
