2b455300b671f2a13212c10cbb26f48214a84591
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2024-03-05 00:17:03 +0800

    UI for performance page
    - able to see both memory graph and speed graph

--- client/src/pages/PerformancePage.tsx
+++ client/src/pages/PerformancePage.tsx
@@ -1,23 +1,47 @@
 import { useEffect, useState } from "react";
 import Header from "../components/Header";
-import Chart from "chart.js/auto"; // Import Chart.js library
+import { Grid, GridItem, Card } from "@chakra-ui/react";
+import GraphComponent from "../components/GraphComponent";
+import { fetchAllAttempts } from "../api/fetchAllAttempts";
 
-interface BenchmarkData {
-    time: number;
+interface AttemptData {
+    speed?: number;
+    memory?: number;
 }
 
-function BenchmarkPage() {
-    
+function PerformancePage() {
+    const [speeds, setSpeeds] = useState<number[]>([]);
+    const [memories, setMemories] = useState<number[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await fetchAllAttempts();
+                const validAttempts = data.data.filter((attempt: AttemptData) => attempt.speed !== undefined || attempt.memory !== undefined);
+                const speedsData = validAttempts.map((attempt: AttemptData) => attempt.speed).filter((speed: number) => !isNaN(speed));
+                const memoriesData = validAttempts.map((attempt: AttemptData) => attempt.memory).filter((memory: number) => !isNaN(memory));
+                setSpeeds(speedsData);
+                setMemories(memoriesData);
+            } catch (error) {
+                console.error("Error fetching attempts:", error);
+            }
+        };
+        fetchData();
+    }, []);
 
     return (
         <>
             <Header />
-            <div>
-                <h2>Code Speed Benchmark</h2>
-                <canvas id="histogramChart" width="400" height="400"></canvas>
-            </div>
+            <Grid templateColumns="1fr 1fr" gap={4}>
+                <GridItem>
+                    <GraphComponent title="Memory Graph" data={memories} />
+                </GridItem>
+                <GridItem>
+                    <GraphComponent title="Speed Graph" data={speeds} />
+                </GridItem>
+            </Grid>
         </>
     );
 }
 
-export default BenchmarkPage;
+export default PerformancePage;
