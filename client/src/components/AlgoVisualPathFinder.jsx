ec0f5b73f861e27984854b513e002d5e4b3f0f22
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2024-03-13 14:36:11 +0800

    Create AlgoVisualPathFinder.jsx

---
+++ client/src/components/AlgoVisualPathFinder.jsx
@@ -0,0 +1,210 @@
+import React from 'react'
+import Grid from '../components/Grid'
+import {astar, bfs, dfs, dijkstra} from '../util/GraphAlgorithms'
+import '../components/Grid.css'
+import {gridIdx} from '../util/GridDraw'
+import Buffer from '../util/Buffer'
+import {mazeRecursiveDiv} from '../util/MazeGenerationAlgorithms'
+import AlgoVisualSortFunctionBar from '../components/AlgoVisualSortFunctionBar'
+
+class AlgoVisualPathFinder extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            visitedNodes:new Set(),
+            pathToTarget:new Set(),
+            wallNodes:new Set(),
+            weakWallNodes:new Set(),
+            buffer:new Buffer([],[]),
+            sourceNode:{x:5, y:20},
+            targetNode:{x:7, y:30},
+            gridSize:{numRows:25, numCols:45},
+            isUpdateSourceNodeMode:false,
+            isUpdateTargetNodeMode:false,
+            selectedAlgo:'BFS',
+            isDrawingMode:false,
+            selectedWallType:'NON-PASSABLE WALL',
+            isRunning:false,
+            isGeneratingMaze:false,
+            delay:100
+        }
+        this.updateSourceNode = this.updateSourceNode.bind(this)
+        this.setUpdateSourceNodeMode = this.setUpdateSourceNodeMode.bind(this)
+        this.updateTargetNode = this.updateTargetNode.bind(this)
+        this.setUpdateTargetNodeMode = this.setUpdateTargetNodeMode.bind(this)
+        this.setAlgo = this.setAlgo.bind(this)
+        this.bufferAlgo = this.bufferAlgo.bind(this)
+        this.reset = this.reset.bind(this)
+        this.setDrawingMode = this.setDrawingMode.bind(this)
+        this.updateDrawnNodes = this.updateDrawnNodes.bind(this)
+        this.setWallType = this.setWallType.bind(this)
+        this.playback = this.playback.bind(this)
+        this.pausePlayback = this.pausePlayback.bind(this)
+        this.start = this.start.bind(this)
+        this.clearVisuals = this.clearVisuals.bind(this)
+        this.generateMaze = this.generateMaze.bind(this)
+        this.setDelay = this.setDelay.bind(this)
+    }
+
+    updateSourceNode(sourceNode) {
+        this.setState({sourceNode:sourceNode, isUpdateSourceNodeMode:false})
+    }
+
+    updateTargetNode(targetNode) {
+        this.setState({targetNode:targetNode, isUpdateTargetNodeMode:false})
+    }
+
+    setUpdateSourceNodeMode() {
+        if (this.state.buffer.isEmpty())
+            this.setState({isUpdateSourceNodeMode:true})
+    }
+
+    setUpdateTargetNodeMode() {
+        if (this.state.buffer.isEmpty())
+            this.setState({isUpdateTargetNodeMode:true})
+    }
+
+    setAlgo(algo) {
+        if (this.state.isRunning) return
+        this.setState({selectedAlgo:algo, visitedNodes:new Set(), pathToTarget:new Set(), buffer: new Buffer([], [])})
+    }
+
+    bufferAlgo() {
+        let buffer = null
+        switch(this.state.selectedAlgo) {
+            case 'BFS':
+                buffer = bfs(this.state.sourceNode, this.state.targetNode, this.state.gridSize, this.state.wallNodes)
+                break
+            case 'DFS':
+                buffer = dfs(this.state.sourceNode, this.state.targetNode, this.state.gridSize, this.state.wallNodes)
+                break
+            case 'DIJKSTRA':
+                buffer = dijkstra(this.state.sourceNode, this.state.targetNode, this.state.gridSize, this.state.wallNodes, this.state.weakWallNodes)
+                break
+            case 'ASTAR':
+                buffer = astar(this.state.sourceNode, this.state.targetNode, this.state.gridSize, this.state.wallNodes, this.state.weakWallNodes)
+                break
+            default:
+                return
+        }
+        buffer.update(this.state.visitedNodes.size)
+        this.setState({buffer:buffer})
+    }
+
+    async start() {
+        if (this.state.isGeneratingMaze || this.state.isRunning) return
+        if (this.state.buffer.isEmpty()) {
+            await(new Promise((resolve) => {
+                this.clearVisuals()
+                resolve()
+            }))
+        }
+        await(new Promise((resolve) => {
+            this.bufferAlgo()
+            resolve()
+        }))
+        this.playback()
+    }
+
+    playback() {
+        const createPromise = (visitedNodes, pathToTarget) => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    this.setState({visitedNodes:visitedNodes, pathToTarget:pathToTarget})
+                    resolve();
+                }, this.state.delay)
+            })
+        }
+        this.setState({isRunning:true}, async ()=> {
+            while (this.state.isRunning) {
+                if (this.state.buffer.visitedIsEmpty() && this.state.buffer.pathIsEmpty()) {
+                    this.setState({isRunning:false})
+                    break
+                }
+                let visitedNodes = new Set(this.state.visitedNodes)
+                let pathToTarget = new Set(this.state.pathToTarget)
+                if (!this.state.buffer.visitedIsEmpty()) {
+                    visitedNodes.add(this.state.buffer.consumeVisited())
+                } else {
+                    pathToTarget.add(this.state.buffer.consumePath())
+                }
+                await(createPromise(visitedNodes, pathToTarget))
+            }
+        })
+    }
+
+    pausePlayback() {
+        this.setState({isRunning:false})
+    }
+
+    setDrawingMode(isDrawingMode) {
+        this.setState({isDrawingMode:isDrawingMode})
+    }
+
+    updateDrawnNodes(nodeIndex) {
+        if (this.state.isRunning) return;
+        let updatedWallNodes = this.state.selectedWallType === 'NON-PASSABLE WALL' ? new Set(this.state.wallNodes) : new Set(this.state.weakWallNodes)
+        updatedWallNodes.add(gridIdx(nodeIndex, this.state.gridSize.numCols))
+        let updatedState = this.state.selectedWallType === 'NON-PASSABLE WALL' ? {wallNodes:updatedWallNodes} : {weakWallNodes:updatedWallNodes}
+        this.setState(updatedState)
+    }
+
+    reset() {
+        this.setState({visitedNodes:new Set(), pathToTarget:new Set(), wallNodes:new Set(), weakWallNodes:new Set(), buffer:new Buffer([], [])})
+    }
+
+    clearVisuals() {
+        this.setState({visitedNodes:new Set(), pathToTarget:new Set(), buffer:new Buffer([], [])})
+    }
+
+    setWallType(wallType) {
+        this.setState({selectedWallType:wallType})
+    }
+
+    setDelay(delay) {
+        this.setState({delay:delay})
+    }
+
+    generateMaze() {
+        if (this.state.isRunning || this.state.isGeneratingMaze) return
+        const createPromise = (wallNodes) => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    this.setState({wallNodes:wallNodes})
+                    resolve();
+                }, this.state.delay)
+            })
+        }
+        const res = mazeRecursiveDiv(this.state.gridSize, this.state.sourceNode, this.state.targetNode)
+        const wallNodes = new Set()
+        this.setState({isGeneratingMaze:true, visitedNodes:new Set(), pathToTarget:new Set(), wallNodes:new Set(), weakWallNodes:new Set()}, async ()=>{
+            for (let wallNode of res) {
+                wallNodes.add(wallNode)
+                await(createPromise(wallNodes))
+            }
+            this.setState({isGeneratingMaze:false})
+        })
+    }
+
+    render() {
+        const nodeModifier = {
+            setUpdateSourceNodeMode:this.setUpdateSourceNodeMode,
+            setUpdateTargetNodeMode:this.setUpdateTargetNodeMode,
+            updateSourceNode:this.updateSourceNode,
+            updateTargetNode:this.updateTargetNode,
+            isUpdateSourceNodeMode:this.state.isUpdateSourceNodeMode,
+            isUpdateTargetNodeMode:this.state.isUpdateTargetNodeMode,
+            setDrawingMode:this.setDrawingMode,
+            isDrawingMode:this.state.isDrawingMode,
+            updateDrawnNodes:this.updateDrawnNodes
+        }
+        return (
+            <div>
+                <AlgoVisualSortFunctionBar genMaze={this.generateMaze} setAlgo={this.setAlgo} setDelay={this.setDelay} start={this.start} pause={this.pausePlayback} reset={this.reset} setWallType={this.setWallType} isRunning={this.state.isRunning} delay={this.state.delay}/>
+                <Grid gridState={this.state} nodeModifier={nodeModifier}/>
+            </div>
+        )
+    }
+}
+
+export default AlgoVisualPathFinder
