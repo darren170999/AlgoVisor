ffae1f7b9f41aaff109d583f216bfa2938a5ffb5
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2024-03-19 15:10:21 +0800

    Batch update

--- client/src/pages/Townhall.tsx
+++ client/src/pages/Townhall.tsx
@@ -1,12 +1,35 @@
-import FullScreenSection from "../components/FullScreenSection";
+import { Box, Text } from "@chakra-ui/react";
 import Header from "../components/Header";
 
-function Visualizer(){
+// Assuming you have an array of URLs stored in another file
+import townHallUtil from "../util/TownHallUtil";
+
+function TownHall(){
+    // Selecting the URL based on some condition, like an index
+    const selectedUrlIndex = "careerFair"; // Change this index based on your condition
+    const selectedUrl = townHallUtil[selectedUrlIndex];
+
     return(
         <>
-        <div>Visualizer</div>
-
+            <Header/>
+            <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                height="50px" // Adjust the height as needed
+                fontFamily="sans-serif"
+                fontSize="3xl" // Adjust the font size as needed
+                fontWeight="bold"
+            >
+                <Text>Town Hall</Text>
+            </Box>
+            <iframe 
+                src={selectedUrl}
+                style={{ width: '100%', height: '768px', padding: '50px' }} 
+                allow="microphone; camera; vr; speaker;"
+            >
+            </iframe>
         </>
     );
 }
-export default Visualizer;
+export default TownHall;
---
+++ client/src/util/TownHallUtil.ts
@@ -0,0 +1,9 @@
+// urls.js
+
+const TownHallUtil = {
+    careerFair: "https://hubs.mozilla.com/79ara2u?embed_token=49444e48443a63e1b060fac3d6fa75d6",
+    url2: "https://hubs.mozilla.com/link/79ara2u",
+    url3: "https://hubs.mozilla.com/link/79ara2u"
+  };
+  
+  export default TownHallUtil;
