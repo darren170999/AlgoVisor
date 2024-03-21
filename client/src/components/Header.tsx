9065cc755b156639755de17d94e0395c1bb0ac29
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2024-03-21 13:17:13 +0800

    Update Header.tsx

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,82 +1,80 @@
+import React, { useState, useEffect } from "react";
 import { Avatar, Box, HStack } from "@chakra-ui/react";
-import { faBook, faDroplet, faEye, faHouse, faLock, faLockOpen } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect, useState } from "react";
+import { SiVisualstudio, SiVisualstudiocode } from "react-icons/si";
+import { RiAdminLine, RiAdminFill } from "react-icons/ri";
+import { GiWhiteBook, GiBlackBook } from "react-icons/gi";
+import { FaRegFileCode, FaFileCode } from "react-icons/fa6";
+import { PiChatsCircleDuotone, PiChatsCircleFill } from "react-icons/pi";
+import { Link } from "react-router-dom";
 import LogOutButton from "./LogOutButton";
 import LoginSignUpButton from "./LoginButton";
-import { Link } from "react-router-dom";
+
+const iconData = [
+  { name: "algovisual", IconSelected: FaFileCode, Icon: FaRegFileCode },
+  { name: "townhall", IconSelected: PiChatsCircleFill, Icon: PiChatsCircleDuotone },
+  { name: "concepts", IconSelected: GiBlackBook, Icon: GiWhiteBook },
+  { name: "tutorials", IconSelected: SiVisualstudio, Icon: SiVisualstudiocode },
+];
+
 function Header() {
-    const [loggedIn, setLoggedIn] = useState<Boolean>(false);
-    const [isSuperAdmin, setIsSuperAdmin] = useState<Boolean>(false);
-    const checkSuperAdmin = () => {
-        if(localStorage.getItem("isSuperAdmin") == 'true'){
-          setIsSuperAdmin(true);
-          return;
-        }
-        else{
-          setIsSuperAdmin(false);
-        }
-        return;    
-    };
-    const checkLoggedIn = () => {
-        console.log(localStorage)
-        if(localStorage.getItem("user") === 'true'){
-            setLoggedIn(true);
-          return;
-        }
-        else{
-            setLoggedIn(false);
-        }
-        return;    
-    };
-    useEffect(()=>{
-        checkSuperAdmin();
-        checkLoggedIn();
-    },[loggedIn])
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [isSuperAdmin, setIsSuperAdmin] = useState(false);
+  const [selectedIcon, setSelectedIcon] = useState<string | null>(null);
 
-    const handleLogout = () => {
-        localStorage.setItem('user','false');
-        localStorage.setItem('isSuperAdmin', 'false');
-    };
-    return (
-        <>
-            <Box position="relative" backgroundColor="#fcc015">
-                <Box color="black" maxWidth="1280px" margin="0 auto" >
-                    <HStack px={18} py={2} justifyContent="space-between" alignItems="center">
-                        <Link to="/home">
-                            <Avatar name="Home" src="apple-touch-icon.png" />
-                        </Link>
-                        <HStack>
-                            {!loggedIn && <LoginSignUpButton />}
-                        </HStack>
-                            {loggedIn && <nav>
-                                <HStack spacing={24} >
-                                    <Link to="/algovisual">
-                                        <FontAwesomeIcon icon={faEye} size="2x" />
-                                    </Link>
-                                    <Link to="/townhall">
-                                        <FontAwesomeIcon icon={faHouse} size="2x" />
-                                    </Link>
-                                    <Link to="/concepts">
-                                        <FontAwesomeIcon icon={faBook} size="2x" />
-                                    </Link>
-                                    <Link to="/tutorials">
-                                        <FontAwesomeIcon icon={faDroplet} size="2x" />
-                                    </Link>
-                                    {isSuperAdmin ? <Link to="/admin">
-                                        <FontAwesomeIcon icon={faLockOpen} size="2x" />
-                                    </Link> : <FontAwesomeIcon icon={faLock} size="2x" />}
+  const checkSuperAdmin = () => {
+    setIsSuperAdmin(localStorage.getItem("isSuperAdmin") === 'true');
+  };
 
-                                    <a> <LogOutButton onClick={handleLogout}/></a>
-                                </HStack>
-                            </nav>}
-                    </HStack>
-                    <HStack>
-                        
-                    </HStack>
-                </Box>
-            </Box>
-        </>
-    );
-};
+  const checkLoggedIn = () => {
+    setLoggedIn(localStorage.getItem("user") === 'true');
+  };
+
+  useEffect(() => {
+    checkSuperAdmin();
+    checkLoggedIn();
+  }, [loggedIn]);
+
+  const handleLogout = () => {
+    localStorage.setItem('user', 'false');
+    localStorage.setItem('isSuperAdmin', 'false');
+  };
+
+  const handleIconClick = (iconName: string ) => {
+    setSelectedIcon(iconName === selectedIcon ? null : iconName);
+  };
+
+  return (
+    <Box position="relative" backgroundColor="#fcc015">
+      <Box color="black" maxWidth="1280px" margin="0 auto">
+        <HStack px={18} py={2} justifyContent="space-between" alignItems="center">
+          <Link to="/home">
+            <Avatar name="Home" src="apple-touch-icon.png" />
+          </Link>
+          <HStack>
+            {!loggedIn && <LoginSignUpButton />}
+          </HStack>
+          {loggedIn && (
+            <nav>
+              <HStack spacing={24}>
+                {iconData.map(({ name, IconSelected, Icon }) => (
+                  <Link to={`/${name}`} key={name}>
+                    {selectedIcon === name ? <IconSelected size="40px" /> : <Icon size="40px" onClick={() => handleIconClick(name)} />}
+                  </Link>
+                ))}
+                {isSuperAdmin && (
+                  <Link to="/admin">
+                    {selectedIcon === 'admin' ? <RiAdminFill size="40px" /> : <RiAdminLine size="40px" onClick={() => handleIconClick('admin')} />}
+                  </Link>
+                )}
+                <LogOutButton onClick={handleLogout} />
+              </HStack>
+            </nav>
+          )}
+        </HStack>
+        <HStack />
+      </Box>
+    </Box>
+  );
+}
+
 export default Header;
