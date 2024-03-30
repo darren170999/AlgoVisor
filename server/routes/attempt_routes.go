ef123d8db80c026b7646229b6644c85986a00c6b
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2024-03-30 12:31:03 +0800

    Update attempt_routes.go

--- server/routes/attempt_routes.go
+++ server/routes/attempt_routes.go
@@ -15,11 +15,11 @@ func AttemptRoute(router *gin.Engine) {
 	router.PUT("/tutorials/code/attempt/status/:qnid/:language/:username", controllers.UpdateStatus())
 	router.GET("/tutorials/code/attempt", controllers.GetAllAttempts())
 	// Swagger API testing
-	router.POST("/api/tutorials/code/attempt/create", controllers.CreateAttempt())
-	router.GET("/api/tutorials/code/attempt/:qnid/:language/:username", controllers.GetAttemptByQnIdByUsername())
-	router.GET("/api/tutorials/code/check/:username", controllers.GetAllAttemptsByUsername())
-	router.PUT("/api/tutorials/code/attempt/:qnid/:language/:username", controllers.UpdateAttempt())
-	router.PUT("/api/tutorials/code/attempt/status/:qnid/:language/:username", controllers.UpdateStatus())
-	router.GET("/api/tutorials/code/attempt", controllers.GetAllAttempts())
+	// router.POST("/api/tutorials/code/attempt/create", controllers.CreateAttempt())
+	// router.GET("/api/tutorials/code/attempt/:qnid/:language/:username", controllers.GetAttemptByQnIdByUsername())
+	// router.GET("/api/tutorials/code/check/:username", controllers.GetAllAttemptsByUsername())
+	// router.PUT("/api/tutorials/code/attempt/:qnid/:language/:username", controllers.UpdateAttempt())
+	// router.PUT("/api/tutorials/code/attempt/status/:qnid/:language/:username", controllers.UpdateStatus())
+	// router.GET("/api/tutorials/code/attempt", controllers.GetAllAttempts())
 
 }
