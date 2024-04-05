3a76ee4488e40b1a7aeabafe2658d8d6a67642fb
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2024-04-05 16:50:42 +0800

    Production

--- client/src/api/submitSourceCode.ts
+++ client/src/api/submitSourceCode.ts
@@ -12,3 +12,32 @@ export async function submitSourceCode(sourceCode: string, languageId: number): 
     throw error; // Rethrow the error for handling in the component
   }
 }
+
+export async function submitSourceCodeProduction(sourceCode: string, languageId: number): Promise<string> {
+  const options = {
+    method: 'POST',
+    url: 'https://judge0-ce.p.rapidapi.com/submissions',
+    params: {
+      base64_encoded: 'true',
+      fields: '*'
+    },
+    headers: {
+      'content-type': 'application/json',
+      'Content-Type': 'application/json',
+      'X-RapidAPI-Key': '03ae996fdbmshbceae746d30896cp102cf4jsne5ad46c7146a',
+      'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
+    },
+    data: {
+      source_code: sourceCode,
+      language_id: languageId,
+    }
+  };
+
+  try {
+    const response = await axios.request(options);
+    return response.data.token;
+  } catch (error) {
+    console.error('Error submitting source code:', error);
+    throw error;
+  }
+}
