07ce66f59b7a868962621f218f904a035f9fb5fe
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2024-04-12 16:16:59 +0800

    Update AlgoVisualFunctionBar.jsx

--- client/src/components/AlgoVisualFunctionBar.jsx
+++ client/src/components/AlgoVisualFunctionBar.jsx
@@ -3,9 +3,40 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
 import RangeSlider from 'react-bootstrap-range-slider';
 import './FunctionBar.css';
-import { Box, Button, Select } from '@chakra-ui/react';
+import { Box, Button, Select, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton, Icon } from '@chakra-ui/react';
+import { InfoOutlineIcon } from '@chakra-ui/icons';
 class AlgoVisualFunctionBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showInstructionsModal: false,
+            currentStep: 0,
+            instructions: [
+                "Click GENERATE to create a maze of your choice.",
+                "SELECT a path finding algorithm.",
+                "RUN or PAUSE the visualization.",
+                "Slide the SPEED meter to adjust the speed of the visualization.",
+                "CODE of the path finding functionality is shown below",
+                "Hit RESET to clear the maze output."
+            ]
+        };
+    }
+
+    handleModalClose = () => {
+        this.setState({ showInstructionsModal: false });
+    };
+
+    handleNextStep = () => {
+        const { currentStep, instructions } = this.state;
+        if (currentStep < instructions.length - 1) {
+            this.setState({ currentStep: currentStep + 1 });
+        } else {
+            this.setState({ showInstructionsModal: false, currentStep: 0 });
+        }
+    };
+
     render() {
+        const { showInstructionsModal, currentStep, instructions } = this.state;
         return (
             <Box className="button_container">
                 <Button onClick={()=>this.props.genMaze()} colorScheme="red"> 
@@ -39,6 +70,22 @@ class AlgoVisualFunctionBar extends React.Component {
                 <Button onClick={()=>this.props.reset()}>
                     RESET
                 </Button>
+                <Button size={20} position="fixed" top={24} right={12} onClick={() => this.setState({ showInstructionsModal: true })}>
+                    <Icon boxSize={10} as={InfoOutlineIcon}/>
+                </Button>
+                <Modal isOpen={showInstructionsModal} onClose={this.handleModalClose}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>Instructions</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            <p>{instructions[currentStep]}</p>
+                            <Button onClick={this.handleNextStep} colorScheme="blue">
+                                {currentStep === instructions.length - 1 ? 'Close' : 'Next'}
+                            </Button>
+                        </ModalBody>
+                    </ModalContent>
+                </Modal>
             </Box>
         )
     }
